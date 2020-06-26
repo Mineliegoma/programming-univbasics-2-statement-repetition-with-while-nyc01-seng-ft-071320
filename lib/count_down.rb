e344e08d15d6359a9536bb43c39fa746fdae7ab1
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-26 15:49:01 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,8 @@
 # Write your code here
-magic_number = 1 
+
 count = 10 
-while count <=1 do
-  break if count == magic_number
-  count = count - 1 
-  
+while count >= 1 do
+  puts count
+  count -= 1
 end
 puts "Happy New Year"
